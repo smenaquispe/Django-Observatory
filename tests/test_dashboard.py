1485f215719cc2f0ecdd7bc6a4e34a4eda1170d7
smenaq <sergisantosmenaq@gmail.com>
2025-11-21 19:53:08 -0500

    Testing Dashboard appears

--- tests/test_dashboard.py
+++ tests/test_dashboard.py
@@ -13,16 +13,13 @@ class TestObservatoryDashboard:
         - Logs
         - Jobs
         """
-        # Make a GET request to the observatory endpoint
+
         response = client.get('/observatory/')
         
-        # Assert the response is successful
         assert response.status_code == 200
         
-        # Decode the response content
         content = response.content.decode('utf-8')
         
-        # Assert that all three tabs are present in the response
         assert 'Requests' in content, "The 'Requests' tab should be present"
         assert 'Logs' in content, "The 'Logs' tab should be present"
         assert 'Jobs' in content, "The 'Jobs' tab should be present"
